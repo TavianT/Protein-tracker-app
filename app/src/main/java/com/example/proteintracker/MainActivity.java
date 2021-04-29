8faba3c40ecf08c2cffb8ee886111be8643fbb94
Tavian Taylor <taviantaylor00@gmail.com>
2021-04-29 10:22:58 +0100

    Action bar set up

--- app/src/main/java/com/example/proteintracker/MainActivity.java
+++ app/src/main/java/com/example/proteintracker/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.proteintracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+//        myToolbar.inflateMenu(R.menu.menu);
+//        myToolbar.setOnMenuItemClickListener(this::onOptionsItemSelected);
+        setSupportActionBar(myToolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_addDailyProtein:
+                Log.d("Action", "onOptionsItemSelected: addDailyProtein selected");
+                break;
+            case R.id.action_addFood:
+                Log.d("Action", "onOptionsItemSelected: addFood selected");
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
