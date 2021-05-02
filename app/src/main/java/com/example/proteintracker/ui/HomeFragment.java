55518b96587d88edb13647f0b1699634239c3482
Tavian Taylor <taviantaylor00@gmail.com>
2021-05-02 13:47:37 +0100

    Pie chart implementation (half working)

--- app/src/main/java/com/example/proteintracker/ui/HomeFragment.java
+++ app/src/main/java/com/example/proteintracker/ui/HomeFragment.java
@@ -4,18 +4,26 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
 import com.example.proteintracker.R;
 import com.example.proteintracker.controller.DailyProteinController;
 import com.example.proteintracker.controller.FoodController;
 import com.example.proteintracker.model.DailyProtein;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,21 +37,15 @@ public class HomeFragment extends Fragment {
     //temp
     final double PROTEIN_TARGET = 150;
     double consumedGrams, remainingGrams;
+    Pie pie;
 
     TextView targetTextView, remainingTextView, consumedTextView;
+    AnyChartView pieChartView;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
@@ -62,9 +64,11 @@ public class HomeFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
         consumedGrams = 0;
         remainingGrams = 0;
+        //pie = AnyChart.pie();
         targetTextView = v.findViewById(R.id.targetGramsTextView);
         remainingTextView = v.findViewById(R.id.remainingGramsTextView);
         consumedTextView = v.findViewById(R.id.consumedGramsTextView);
+        pieChartView = v.findViewById(R.id.pieChart);
         List<DailyProtein> dpList = new ArrayList<DailyProtein>();
         DailyProteinController dpController = new DailyProteinController(requireContext());
         dpList = dpController.getCurrentDailyProtein();
@@ -73,10 +77,54 @@ public class HomeFragment extends Fragment {
             double protein = foodController.getProteinById(dp.foodId);
             consumedGrams += protein;
         }
-        remainingGrams = PROTEIN_TARGET - consumedGrams;
-        targetTextView.setText(String.valueOf(PROTEIN_TARGET) + " g");
-        remainingTextView.setText(String.valueOf(remainingGrams) + " g");
-        consumedTextView.setText(String.valueOf(consumedGrams) + " g");
+        if (consumedGrams < PROTEIN_TARGET) {
+            remainingGrams = PROTEIN_TARGET - consumedGrams;
+        } else {
+            remainingGrams = 0;
+        }
+        Log.d("remaining", String.valueOf(remainingGrams));
+        Log.d("consumed", String.valueOf(consumedGrams));
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        String targetString = String.valueOf(decimalFormat.format(PROTEIN_TARGET)) + " g";
+        String remainingString = String.valueOf(decimalFormat.format(remainingGrams)) + " g";
+        String consumedString = String.valueOf(decimalFormat.format(consumedGrams)) + " g";
+        targetTextView.setText(targetString);
+        remainingTextView.setText(remainingString);
+        consumedTextView.setText(consumedString);
+        boolean pieDone = setPieChart();
+        if (pieDone) {
+            Log.d("pie done", String.valueOf(pieDone));
+        }
+        //updatePieChart();
         return v;
+    }
+
+    private boolean setPieChart() {
+        pie = AnyChart.pie();
+        List<DataEntry> dataEntries = new ArrayList<DataEntry>();
+        Log.d("remaining in pie", String.valueOf(remainingGrams));
+        Log.d("consumed in pie", String.valueOf(consumedGrams));
+        dataEntries.add(new ValueDataEntry("Consumed", consumedGrams));
+        dataEntries.add(new ValueDataEntry("Remaining", remainingGrams));
+        pie.data(dataEntries);
+        pie.labels().enabled(false);
+        //pie.tooltip().enabled(false);
+        pie.legend().enabled(false);
+        pie.outline().enabled(false);
+
+        pieChartView.setChart(pie);
+        return true;
+    }
+
+    private void updatePieChart() {
+        final int delay = 500;
+        final Handler handler = new Handler();
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                setPieChart();
+            }
+        };
+        handler.postDelayed(runnable, delay);
     }
 }
