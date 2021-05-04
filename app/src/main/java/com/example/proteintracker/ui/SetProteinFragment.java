833f96b2bce312baa23d39bc99852a44bca743a5
Tavian Taylor <taviantaylor00@gmail.com>
2021-05-04 19:28:08 +0100

    finished set protein fragment implementation

--- app/src/main/java/com/example/proteintracker/ui/SetProteinFragment.java
+++ app/src/main/java/com/example/proteintracker/ui/SetProteinFragment.java
@@ -1,14 +1,21 @@
 package com.example.proteintracker.ui;
 
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.text.InputFilter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.proteintracker.R;
+import com.example.proteintracker.ui.adapter.DecimalDigitsInputFilter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,14 +24,8 @@ import com.example.proteintracker.R;
  */
 public class SetProteinFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    EditText proteinEditText;
+    Button submitButton;
 
     public SetProteinFragment() {
         // Required empty public constructor
@@ -41,10 +42,6 @@ public class SetProteinFragment extends Fragment {
     // TODO: Rename and change types and number of parameters
     public static SetProteinFragment newInstance(String param1, String param2) {
         SetProteinFragment fragment = new SetProteinFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
@@ -52,8 +49,7 @@ public class SetProteinFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
@@ -61,6 +57,19 @@ public class SetProteinFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_set_protein, container, false);
+        View v = inflater.inflate(R.layout.fragment_set_protein, container, false);
+        proteinEditText = v.findViewById(R.id.setProteinEditText);
+        proteinEditText.setFilters(new InputFilter[] {new DecimalDigitsInputFilter(5,2)});
+        submitButton = v.findViewById(R.id.submitProteinButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double protein = Double.parseDouble(proteinEditText.getText().toString().trim());
+                Log.d("protein", String.valueOf(protein)); //testing
+                Navigation.findNavController(requireView()).navigate(R.id.action_setProteinFragment_to_homeFragment);
+            }
+        });
+        return v;
     }
 }
